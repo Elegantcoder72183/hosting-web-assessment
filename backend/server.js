f2d7687030cd454ec2ad3de231118b1e78514b4e
Elegantcoder72183 <114733458+Elegantcoder72183@users.noreply.github.com>
2026-05-25 11:00:25 +0530

    cors updated

--- backend/server.js
+++ backend/server.js
@@ -13,13 +13,7 @@ connectDB();
 
 const app = express();
 
-app.use(
-  cors({
-    origin: [
-      "http://localhost:5173",
-    ],
-  })
-);
+app.use(cors());
 app.use(express.json());
 
 app.use("/api", deploymentRoutes);
